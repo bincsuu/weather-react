c0dc04e6a1cabc8df9a28c2b93bbcc14371ef7a6
bincsuu <bianca.sero01@gmail.com>
2023-01-23 12:46:02 +0100

    bootstrap installed

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="Weather App">
+      <div className="container">
       <h1>Weather App</h1>
       <WeatherSearch />
+      </div>
     </div>
   );
 }
